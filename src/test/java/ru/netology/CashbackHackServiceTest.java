d1113e985a645602bb224ce93d0149561387eb6d
Olga Sukhareva <3830921@gmail.com>
2025-06-22 14:10:28 +0300

    addm testng

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,54 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void testTheAmountIsNotEnough() {
+        int amount = 20;
+
+        int actual = service.remain(amount);
+        int expected = 980;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testTheAmountGreaterThanMinimum() {
+        int amount = 1999;
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testTheAmountIsMinimum() {
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testTheAmountIsSufficient2() {
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testTheAmountIsSufficient() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+
+    }
+
+
+}
